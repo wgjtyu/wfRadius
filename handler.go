8d44895ba6d1bf6a05a9edf34c2cb56dbde8c94c
George Wang <wgjtyu@gmail.com>
2020-12-27 15:40:27 +0800

    打印更多日志

--- handler.go
+++ handler.go
@@ -3,13 +3,11 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
-	"log"
-
 	badger "github.com/dgraph-io/badger/v2"
 	"go.uber.org/zap"
 	"layeh.com/radius"
 	"layeh.com/radius/rfc2865"
+	"layeh.com/radius/rfc2866"
 )
 
 func handler(w radius.ResponseWriter, r *radius.Request) {
@@ -19,12 +17,21 @@ func handler(w radius.ResponseWriter, r *radius.Request) {
 	port := rfc2865.LoginTCPPort_Get(r.Packet)
 	srv := rfc2865.LoginService_Get(r.Packet)
 	srvType := rfc2865.ServiceType_Get(r.Packet)
+	callingStationID := rfc2865.CallingStationID_GetString(r.Packet) // 客户端Mac地址
+	calledStationID := rfc2865.CalledStationID_GetString(r.Packet) // AP的MAC地址
+	framedIpAddress := rfc2865.FramedIPAddress_Get(r.Packet) // 客户端的IP地址
+	acctSessionId := rfc2866.AcctSessionID_GetString() // 计费会话ID
+
 
 	zap.L().Info("handler-用户请求信息", zap.String("Code", r.Code.String()),
 		zap.String("IP", ip.String()),
 		zap.String("Port", port.String()),
 		zap.String("Srv", srv.String()),
 		zap.String("SrvType", srvType.String()),
+		zap.String("Calling-Station-Id", callingStationID),
+		zap.String("Called-Station-Id", calledStationID),
+		zap.String("Framed-IP-Address", framedIpAddress.String()),
+		zap.String("Acct-Session-Id", acctSessionId),
 		zap.String("UserId", username),
 		zap.String("Password", password),
 		zap.String("LocalAddr", r.LocalAddr.String()),
@@ -35,7 +42,6 @@ func handler(w radius.ResponseWriter, r *radius.Request) {
 		var key bytes.Buffer
 		key.WriteString("ID")
 		key.WriteString(username)
-		fmt.Printf("Key=%s\n", key.String())
 		item, err := txn.Get(key.Bytes())
 		if err != nil {
 			zap.L().Warn("handler-txn.Get出错", zap.String("error", err.Error()))
@@ -73,6 +79,8 @@ func handler(w radius.ResponseWriter, r *radius.Request) {
 		}
 	}
 
-	log.Printf("Writing %v to %v", code, r.RemoteAddr)
+	zap.L().Info("Handler finished",
+		zap.String("code", code.String()),
+		zap.String("remoteAddr", r.RemoteAddr.String()))
 	w.Write(r.Response(code))
 }
