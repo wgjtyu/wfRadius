50a97016a1fb1b75f284fe4c45d671aaf2869053
George Wang <wgjtyu@gmail.com>
2022-05-05 15:56:31 +0800

    改造upload_log

--- src/wifilog/upload_log.go
+++ src/wifilog/upload_log.go
@@ -1,24 +1,34 @@
 package wifilog
 
 import (
+	"go.uber.org/zap"
+	"gorm.io/gorm"
 	"time"
 	"wfRadius/model"
 	"wfRadius/src/request"
-	"wfRadius/storage"
-
-	"go.uber.org/zap"
 )
 
-func uploadLog() {
+type Uploader struct {
+	db     *gorm.DB
+	logger *zap.Logger
+}
+
+func NewUploader(db *gorm.DB, l *zap.Logger) *Uploader {
+	u := &Uploader{db: db, logger: l.Named("WifiLogUploader")}
+	go u.beginTask()
+	return u
+}
+
+func (u *Uploader) uploadLog() {
 	var logs []model.MWifiLog
-	err := storage.DB.Find(&logs).Error
+	err := u.db.Find(&logs).Error
 	if len(logs) == 0 {
 		return
 	}
 	if err != nil {
-		zap.L().Error("uploadLog-获取所有wifi认证日志出错", zap.Error(err))
+		u.logger.Error("uploadLog-获取所有wifi认证日志出错", zap.Error(err))
 	} else {
-		zap.L().Info("uploadLog-上传Wifi验证记录", zap.Int("count", len(logs)))
+		u.logger.Info("uploadLog-上传Wifi验证记录", zap.Int("count", len(logs)))
 		resp := request.Post("/api/wifi_code/save_log", logs)
 
 		// 删除已经上传的记录
@@ -27,15 +37,16 @@ func uploadLog() {
 			for _, l := range logs {
 				ids = append(ids, l.ID)
 			}
-			err = storage.DB.Delete(&model.MWifiLog{}, ids).Error
+			err = u.db.Delete(&model.MWifiLog{}, ids).Error
 			if err != nil {
-				zap.L().Error("删除wifiLog出错", zap.Error(err))
+				u.logger.Error("删除wifiLog出错", zap.Error(err))
 			}
 		}
 	}
 }
 
-func BeginUploadTask() {
+// FIXME 改造成RateLimiter, 并在Shutdown时全部上传
+func (u *Uploader) beginTask() {
 	d := time.Second * 300
 
 	t := time.NewTicker(d)
@@ -43,6 +54,10 @@ func BeginUploadTask() {
 
 	for {
 		<-t.C
-		uploadLog()
+		u.uploadLog()
 	}
+}
+
+func (u *Uploader) Shutdown() {
+	// FIXME implement it
 }
